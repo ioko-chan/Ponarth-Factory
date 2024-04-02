5c67360885d9ab2919399d8d3fc45068318aed81
ioko-chan <kvotua.job@gmail.com>
2024-04-02 22:16:21 +0200

    disable cors

--- src/main/java/com/solomennicova/AuthTemplate/Security/Filter/CorsFilter.java
+++ src/main/java/com/solomennicova/AuthTemplate/Security/Filter/CorsFilter.java
@@ -14,6 +14,7 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Authorization", "**");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS");
         response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
